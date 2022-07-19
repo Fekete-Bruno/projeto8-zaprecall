0fab28e3282449df77d7646011607e04f95466fe
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-19 12:50:40 -0300

    Title-screen css ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 export default function App(){
     return(
         <div class="content">
-            
+            <div class="title-screen">
+            <img src="./assets/logo-new.png"></img>
+            <h1>ZapRecall</h1>
+            <div class="button">Iniciar Recall!</div>
+            </div>
         </div>     
     )
 }
