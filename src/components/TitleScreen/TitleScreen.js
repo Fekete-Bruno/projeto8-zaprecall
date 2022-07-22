862818bae8428c9369b09c3b6aba35b877f0962a
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-22 20:57:58 -0300

    select stile ready

--- src/components/TitleScreen/TitleScreen.js
+++ src/components/TitleScreen/TitleScreen.js
@@ -10,13 +10,13 @@ export default function TitleScreen({decks,setScreen,deckNumber,setDeckNumber}){
     return(
         <div className="title">
             <img src={newLogo} alt="new logo"></img>
+            <div className="title-text">ZapRecall</div>
             <select required defaultValue={deckNumber} onChange={(e)=>{setDeckNumber(e.target.value)}}>
             <option value="" disabled >Selecione o seu deck</option>
             {decks.map((deck,index)=>{
                 return(<option value={index} key={index}>{deck.deckName}</option>);
             })}
             </select>
-            <div className="title-text">ZapRecall</div>
             <div className="button" onClick={startRecall}>Iniciar Recall!</div>
         </div>
     );
