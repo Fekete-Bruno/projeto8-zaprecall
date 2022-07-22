7e585266fb00f24545cf86145e83e5dedd402a11
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-22 20:45:30 -0300

    Deck select logic ok

--- src/components/DeckScreen.js
+++ src/components/DeckScreen.js
@@ -3,17 +3,7 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import Cards from './Cards'
 
-export default function DeckScreen({setScreen}){
-    const decks = [
-        {question:"O que é JSX?", answer:"Uma extensão de linguagem do JavaScript"},
-        {question:"O React é ___", answer:"uma biblioteca JavaScript para construção de interfaces"},
-        {question:"Componentes devem começar com ___", answer: "letra maiúscula"},
-        {question:"Podemos colocar ___ dentro do JSX", answer:"expressões" },
-        {question:"O ReactDOM nos ajuda ___", answer:"interagindo com a DOM para colocar componentes React na mesma" },
-        {question:"Usamos o npm para ___", answer:"gerenciar os pacotes necessários e suas dependências" },
-        {question:"Usamos props para ___", answer:"passar diferentes informações para componentes" },
-        {question:"Usamos estado (state) para ___", answer:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" },
-    ]
+export default function DeckScreen({questions,setScreen}){
 
     const [started,setStarted] = React.useState(false)
     const [deck,setDeck] = React.useState([]);
@@ -21,9 +11,9 @@ export default function DeckScreen({setScreen}){
     const [footerIcons, setFooterIcons] = React.useState([]);
 
     function createDeck() {
-    decks.sort(() => Math.random() - 0.5);
-    decks.forEach((card) => {
-        deck.push({ ...card,
+    questions.sort(() => Math.random() - 0.5);
+    questions.forEach((question) => {
+        deck.push({ ...question,
         status:"card"});
     });
     }
--- src/components/TitleScreen/TitleScreen.js
+++ src/components/TitleScreen/TitleScreen.js
@@ -1,12 +1,23 @@
 import "./style.css"
 import newLogo from "./logo-new.png"
 
-export default function TitleScreen({setScreen}){
+export default function TitleScreen({decks,setScreen,deckNumber,setDeckNumber}){
+    function startRecall(){
+        if(deckNumber!==""){
+        setScreen(false);
+        }
+    }
     return(
         <div className="title">
             <img src={newLogo} alt="new logo"></img>
+            <select required defaultValue={deckNumber} onChange={(e)=>{setDeckNumber(e.target.value)}}>
+            <option value="" disabled >Selecione o seu deck</option>
+            {decks.map((deck,index)=>{
+                return(<option value={index} key={index}>{deck.deckName}</option>);
+            })}
+            </select>
             <div className="title-text">ZapRecall</div>
-            <div className="button" onClick={()=>setScreen(false)}>Iniciar Recall!</div>
+            <div className="button" onClick={startRecall}>Iniciar Recall!</div>
         </div>
     );
 }
